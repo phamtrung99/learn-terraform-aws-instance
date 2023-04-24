0e816ca1a09fe09c05df787e1e826a38bac1801e
Trung Pham <trung.pham@executionlab.asia>
2023-04-24 15:39:01 +0700

    Update

--- main.tf
+++ main.tf
@@ -19,14 +19,14 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "nobita_server" {
-  ami                    = "ami-0a3eb6ca097b78895"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = ["sg-090b5e8a1c83eeba4"]
-  subnet_id              = "subnet-03f58e506d9c18ad6"
+# resource "aws_instance" "nobita_server" {
+#   ami                    = "ami-0a3eb6ca097b78895"
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = ["sg-090b5e8a1c83eeba4"]
+#   subnet_id              = "subnet-03f58e506d9c18ad6"
 
-  tags = {
-    Name = var.instance_name
-  }
-}
+#   tags = {
+#     Name = var.instance_name
+#   }
+# }
 
